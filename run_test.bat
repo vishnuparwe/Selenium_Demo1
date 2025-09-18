bed0b9b99a2244e078ea04d961144340ddd34b61
VishnuParwe <parwevishnu@gmail.com>
2025-09-18 17:40:18 +0530

    my first commit

---
+++ run_test.bat
@@ -0,0 +1,2 @@
+C:\Users\dell\IdeaProjects\Selenium_Demo1
+mvn test
---
+++ src/test/java/org/example/Test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Its a demo project");
+    }
+
+}
